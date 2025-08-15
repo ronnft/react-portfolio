d500968098b7157ea29a34d4abd670735fc87e0c
ronnft <ronkrisft@gmail.com>
2025-08-15 23:33:21 +0800

    another test for a change

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -8,7 +8,7 @@ export const Contact = () => {
     <footer id='contact' className={styles.container}>
         <div className={styles.text}>
             <h2>Contact</h2>
-            <p>Reach out to me!</p>
+            <p>Feel free to reach out!</p>
         </div>
         <ul className={styles.links}>
             <li className={styles.link}>
